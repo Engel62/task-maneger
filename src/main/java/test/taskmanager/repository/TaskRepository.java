9b5e4b45e0c06a90fe4f447e9bcff21f50b1f50f
Engel62 <sr.engel@bk.ru>
2026-04-08 15:49:47 +0300

    Исправил репозиторий

--- src/main/java/test/taskmanager/repository/TaskRepository.java
+++ src/main/java/test/taskmanager/repository/TaskRepository.java
@@ -1,11 +1,12 @@
 package test.taskmanager.repository;
 
-import org.hibernate.query.Page;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import test.taskmanager.model.Task;
 
-import java.awt.print.Pageable;
-
+@Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
     Page<Task> findAll(Pageable pageable);
 }
--- src/main/java/test/taskmanager/repository/UserRepository.java
+++ src/main/java/test/taskmanager/repository/UserRepository.java
@@ -1,10 +1,11 @@
 package test.taskmanager.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import test.taskmanager.model.User;
 
 import java.util.Optional;
-
+@Repository
 public interface UserRepository extends JpaRepository<User,Long> {
     Optional<User> findByEmail(String email);
 }
